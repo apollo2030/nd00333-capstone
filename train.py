458d0fb1e6c009702fe18cf0db2d7e6a97e0f06c
Alexandru Dobinda <alexandru.dobinda@mstack.nl>
2021-02-09 23:06:29 +0100

    work in progress

--- train.py
+++ train.py
@@ -1,73 +1,74 @@
+from skits.preprocessing import HorizonTransformer
+
+from sklearn.linear_model import LinearRegression
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.pipeline import FeatureUnion
+
+from skits.feature_extraction import AutoregressiveTransformer
+from skits.pipeline import ForecasterPipeline
+from skits.preprocessing import ReversibleImputer
+from sklearn.metrics import mean_squared_error
+
+from azureml.core import Workspace, Dataset
+from azureml.core.run import Run
+
 import argparse
 import numpy as np
-import pandas as pd
 import pickle
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from azureml.core.run import Run
-from azureml.data.dataset_factory import TabularDatasetFactory
-from azureml.core import Workspace, Dataset
 
 ws = Workspace.from_config()
-ds = Dataset.get_by_name(ws, name='covid-19-cases-deaths-weekly')
-
+ds = Dataset.get_by_name(ws, name='covid-19-measures-cases-weekly')
+df = ds.to_pandas_dataframe()
 run = Run.get_context()
 
-def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
+# number of weeks to predict in the future
+param_horizon = 10
+# number of previous weeks to use as features
+param_datapoints_in_past_as_features = 5
+param_n_jobs = 16
+param_n_estimators = 10
+param_test_size = 10
 
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-    
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    return x_df, y_df
-
-
-x, y = clean_data(ds)
-
-x_train, x_test, y_train, y_test = train_test_split(x, y)
+y = df[df['country']=='Netherlands']['rate_14_day'].values
+X = y.reshape(-1, 1).copy()
 
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--param_horizon', type=int, default=2, help="The horizon in the future to predict in weeks")
+    parser.add_argument('--param_datapoints_in_past_as_features', type=int, default=23, help="Number of past observations to turn into features")
+    parser.add_argument('--param_test_size', type=int, default=10, help="Number of test samples")
+
+    parser.add_argument('--param_n_jobs', type=int, default=16, help="Number of parallel jobs to run")
+    parser.add_argument('--param_n_estimators', type=int, default=10, help="Number of estimators in Xgboost estimator")
     parser.add_argument('--save', type=bool, default=False, help="Save the model")
 
     args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
+    lin_pipeline = ForecasterPipeline([
+        # Convert the `y` target into a horizon
+        ('pre_horizon', HorizonTransformer(horizon=args.param_horizon)),
+        ('pre_reversible_imputer', ReversibleImputer(y_only=True)),
+        ('features', FeatureUnion([
+            ('ar_features', AutoregressiveTransformer(num_lags=args.param_datapoints_in_past_as_features)),
+        ])),
+        ('post_feature_imputer', ReversibleImputer()),
+        ('regressor', MultiOutputRegressor(LinearRegression(fit_intercept=False), n_jobs=args.param_n_jobs))
+    ])
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    train_size = len(X) - args.param_test_size
 
-    accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
+    lin_pipeline = lin_pipeline.fit(X[:train_size], y[:train_size])
+    lin_prediction = lin_pipeline.predict(X, start_idx=train_size)
+
+    y_actual = lin_pipeline.transform_y(X)
+    
+    
+    mrse = mean_squared_error(y_actual[-args.param_test_size:], lin_prediction)
+
+    run.log("MRSE", np.float(mrse))
     if  args.save==True:
-        pickle.dump(model, open( "outputs/model.pkl", "wb" ) )
+        pickle.dump(lin_pipeline, open( "outputs/model.pkl", "wb" ) )
 
 if __name__ == '__main__':
     main()
